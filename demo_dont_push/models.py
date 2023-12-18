58b2ad513dcb2e52f41a83b2df6057a3ece8abfe
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-12-18 22:51:23 +0530

    Update models.py

--- demo_dont_push/models.py
+++ demo_dont_push/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 db = SQLAlchemy()
-
+#comment
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
